01da84a1279deda5424c786a38b4f5b2d8089976
Eric Jin <ericjin2@illinois.edu>
2020-07-18 21:36:11 -0700

    Complete LastValueModel Methods and Documentation

--- app/src/main/java/com/example/finance_stock_inquisitor/LastValueModel.java
+++ app/src/main/java/com/example/finance_stock_inquisitor/LastValueModel.java
@@ -1,4 +1,16 @@
 package com.example.finance_stock_inquisitor;
 
+/**
+ * This is a simple benchmark model which uses the last observed stock price as the
+ * next predicted value.
+ */
 public class LastValueModel {
+    /**
+     *
+     * @param latestPrice - The last observed price of a stock.
+     * @return The predicted price of the stock in the next interval.
+     */
+    public double getNextPredictedPrice(double latestPrice) {
+        return latestPrice;
+    }
 }
